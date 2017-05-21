04a36aa1df6c95383590258e6bc3f550c38ea9a4
jespo <jesposito@bhmi.com>
2017-05-20 19:12:54 -0500

    nifty reference counters

--- JLib/misc.h
+++ JLib/misc.h
@@ -8,6 +8,48 @@
 #include <string.h>
 #include <sstream>
 
+
+//
+//	define used in a .h file to declare a global static variable whose construction and
+//	destruction or is controlled by a nifty reference counter
+//
+//	vClass	is the class of the variable
+//	vVar	is the name of the variable
+//
+#define GLOBAL_DCL( vClass, vVar )			\
+	extern vClass&			vVar;			\
+	static struct C##vVar##_Initializer		\
+	{										\
+		C##vVar##_Initializer();			\
+		~C##vVar##_Initializr();			\
+	} g##vVar##_Initializer;				
+
+//
+//	define used in a .cpp file to construct a global object before its first use
+//	and destruct it after all other static objects that use it are destructed
+//
+//	vNameSpace		is the namespace of the object's class
+//	vClass			is the class of the object
+//	vVar			is the variable name of the object
+//
+#define GLOBAL_IMPL( vNameSpace, vClass, vVar )								\
+	static int vNameSpace##vVar##_nifty_counter;							\
+	static typename std::aligned_storage<sizeof(vNamespace##::##vClass),	\
+		alignof(vNamespace##::##vClass)>::type vNameSpace##vVar##_buf;		\
+	vNamespace::vClass& vNameSpace##::##vVar =								\
+		reinterpret_cast<vNameSpace##::##vVar&>(CTrace_buf);				\
+	vNameSpace##::C##vVar##_Initializer()									\
+	{
+	if (nifty_counter++ == 0)
+	new (&jlib::gTrace) CTrace();
+	}
+
+	jlib::CJlibInitializer::~CJlibInitializer()
+	{
+		if (--nifty_counter == 0)
+			(&jlib::gTrace)->~CTrace();
+	}
+
 namespace jlib
 {
 
--- JLib/trace.cpp
+++ JLib/trace.cpp
@@ -1,4 +1,3 @@
-#include <new>
 #include "trace.h"
 
 //
