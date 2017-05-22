30b79c2a72be16a1e32b4885bdc3cfc62d2de905
jaespo <jeffery.a.esposito@gmail.com>
2017-05-22 17:03:04 -0500

    nifty for statics

--- JLib/nifty.h
+++ JLib/nifty.h
@@ -11,13 +11,26 @@
 //	vClass	is the class of the variable
 //	vVar	is the name of the variable
 //
+//	Example of use:
+//
+//	namespace myNameSpace
+//	{
+//		class CMyClass
+//		{
+//		public:
+//			void SayHello();
+//		};
+//		NIFTY_DCL(CMyClass, gMyGlobal)
+//	}
+//
 #define NIFTY_DCL( vClass, vVar )			\
 	extern vClass&			vVar;			\
 	static struct C##vVar##_Initializer		\
 	{										\
 		C##vVar##_Initializer();			\
 		~C##vVar##_Initializer();			\
-	} ##vVar##_Initializer;				
+	} vVar##_Initializer;				
+
 
 //
 //	define used in a .cpp file to construct a global object before its first use
@@ -39,6 +52,62 @@
 			new (&vNameSpace##_##vVar##_buf) vClass();							\
 	}																			\
 	vNameSpace##::C##vVar##_Initializer::~C##vVar##_Initializer()				\
+	{																			\
+		if (--vNameSpace##_##vVar##_nifty_counter == 0)							\
+			(&vVar)->~vClass();													\
+	}
+
+//
+//	This define is similar to NIFTY_DCL, except that it is used for declaring a 
+//	"global" that is static class member.
+//
+//	vClass	is the class of the variable
+//	vVar	is the name of the variable
+//
+//	Example of use:
+//
+//	namespace myNameSpace1
+//	{
+//		class CMyClass1
+//		{
+//		public:
+//			void SayHello();
+//		private:
+//			static CMyClass1		mgInst;
+//		};
+//		NIFTY_STATIC_DCL(CMyClass1, mgInst)
+//	}
+//
+#define NIFTY_STATIC_DCL( vClass, vVar )		\
+	static vClass	vVar;						\
+	static struct C##vVar##_Initializer			\
+	{											\
+		C##vVar##_Initializer();				\
+		~C##vVar##_Initializer();				\
+	} vVar##_Initializer;			
+
+//
+//	define used in a .cpp file to construct a global object before its first use
+//	and destruct it after all other static objects that use it are destructed
+//
+//	vNameSpace		is the namespace of the object's class
+//	vClass			is the class vof the object
+//	vVar			is the variable name of the object
+//
+#define NIFTY_STATIC_IMPL( vNameSpace, vClass, vVar )							\
+	static int vNameSpace##_##vVar##_nifty_counter;								\
+	static typename std::aligned_storage<sizeof(vNameSpace##::##vClass),		\
+		alignof(vNameSpace##::##vClass)>::type									\
+			vNameSpace##_##vClass##_##vVar##_buf;								\
+	vNameSpace::vClass vNameSpace##::vClass##::##vVar =							\
+		reinterpret_cast<vNameSpace##::##vClass&>(								\
+			vNameSpace##_##vClass##_##vVar##_buf);								\
+	vNameSpace##::vClass::C##vVar##_Initializer::C##vVar##_Initializer()		\
+	{																			\
+		if (vNameSpace##_##vVar##_nifty_counter++ == 0)							\
+			new (&vNameSpace##_##vClass##_##vVar##_buf) vClass();				\
+	}																			\
+	vNameSpace##::vClass::C##vVar##_Initializer::~C##vVar##_Initializer()		\
 	{																			\
 		if (--vNameSpace##_##vVar##_nifty_counter == 0)							\
 			(&vVar)->~vClass();													\
